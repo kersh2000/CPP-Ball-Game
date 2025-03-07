c149405f0962e34db0181ad6fd29b9b24c3429fa
kersh2000 <bradleykershaw333@gmail.com>
2025-03-07 14:53:31 +0000

    Add drag effect to slow player down.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.5f;  // Base movement speed
+    public float speed = 0.5f;  // Base movement speed of the ball
+    public float dragForce = 0.65f;  // Base drag to slow the ball down
 
-    private Rigidbody ball; // Reference to rigibody
+    private Rigidbody ball; // Reference to ball's rigibody
     
     void Start()
     {
         ball = GetComponent<Rigidbody>();
+        // Add drag to ball properties
+        ball.linearDamping = dragForce;
     }
     
     void Update()
@@ -17,7 +20,7 @@ public class PlayerController : MonoBehaviour
         float verticalAxis = Input.GetAxis("Vertical"); // -1 to 1 of vertcal input (S to W)
         float horizontalAxis = Input.GetAxis("Horizontal"); // -1 to 1 of horizontal input (A to D)
 
-        // Add force to rigid body
+        // Add force to ball
         ball.AddForce(speed * horizontalAxis, 0, speed * verticalAxis);
     }
 }
