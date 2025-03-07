9045e8a8a2d30371a11b510db40d7b07576ae4a5
kersh2000 <bradleykershaw333@gmail.com>
2025-03-07 14:48:15 +0000

    Add movement to character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 0.5f;  // Base movement speed
+
+    private Rigidbody ball; // Reference to rigibody
+    
     void Start()
     {
-        
+        ball = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        // Get directional input
+        float verticalAxis = Input.GetAxis("Vertical"); // -1 to 1 of vertcal input (S to W)
+        float horizontalAxis = Input.GetAxis("Horizontal"); // -1 to 1 of horizontal input (A to D)
+
+        // Add force to rigid body
+        ball.AddForce(speed * horizontalAxis, 0, speed * verticalAxis);
     }
 }
