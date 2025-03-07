84990945705db3d35726bd10ab202645839e7fdc
kersh2000 <bradleykershaw333@gmail.com>
2025-03-07 17:50:01 +0000

    Add camera directional movement

--- Assets/Scripts/CameraControlller.cs
+++ Assets/Scripts/CameraControlller.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class CameraControlller : MonoBehaviour
 {
     public Vector3 offset = new Vector3(0, 2.5f, -3.5f);    // Offset from ball
+    public float turnSpeed = 2.0f;
 
     private Rigidbody ball; // Reference to ball
+    private Vector3 currentDirection = Vector3.forward;
 
     void Start()
     {
@@ -14,7 +16,19 @@ public class CameraControlller : MonoBehaviour
 
     void Update()
     {
-        // Add offset from ball's position
-        transform.position = ball.position + offset;
+        // Get ball's direction
+        Vector3 targetDirection = new Vector3(ball.linearVelocity.x, 0, ball.linearVelocity.z).normalized;
+
+        // Delay the new direction vector 
+        currentDirection = Vector3.Slerp(currentDirection, targetDirection, Time.deltaTime * turnSpeed);
+
+        // Calcululate new camera position by adding the ball's position by the multiple of the change in direction by the offset (camera always behind the ball)
+        Vector3 targetPosition = ball.position + Quaternion.LookRotation(targetDirection) * offset;
+
+        // Transition to new position
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * turnSpeed);
+
+        // Look at center of ball for rotation
+        transform.LookAt(ball.position);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
     public float dragForce = 0.65f;  // Base drag to slow the ball down
 
     private Rigidbody ball; // Reference to ball's rigibody
+    private Camera camera;  // Reference to the main camera
     
     void Start()
     {
         ball = GetComponent<Rigidbody>();
+        camera = Camera.main;
         // Add drag to ball properties
         ball.linearDamping = dragForce;
 
@@ -23,8 +25,11 @@ public class PlayerController : MonoBehaviour
         float verticalAxis = Input.GetAxis("Vertical"); // -1 to 1 of vertcal input (S to W)
         float horizontalAxis = Input.GetAxis("Horizontal"); // -1 to 1 of horizontal input (A to D)
 
+        // Find the vector from camera to input
+        Vector3 direction = (camera.transform.TransformDirection(horizontalAxis, 0, verticalAxis));
+
         // Add force to ball
-        ball.AddForce(speed * horizontalAxis, 0, speed * verticalAxis);
+        ball.AddForce(speed * direction.x, 0, speed * direction.z);
     }
 
     void DebugLog()
